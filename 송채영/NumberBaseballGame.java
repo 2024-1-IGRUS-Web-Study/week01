b892246f2918cd832c1741eda871e36f9b577902
buddle031 <121237445+buddle031@users.noreply.github.com>
2024-03-30 16:58:47 +0900

    Update NumberBaseballGame.java

--- 송채영/NumberBaseballGame.java
+++ 송채영/NumberBaseballGame.java
@@ -1,53 +1,51 @@
 import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Calculator {
+public class NumberBaseballGame {
     public static void main(String[] args) {
-        int p1, p2;
-        int operator=0;
-
-        while (operator != 6) {
         Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.println("3자리 숫자를 입력해주세요>> ");
+            String num = sc.nextLine();
+            if (num.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+            if (num.length() != 3) {
+                return;
+            }
 
-        System.out.println("수행하고싶은 연산을 입력해주세요>> ");
-        operator = sc.nextInt();
-        if(operator==6){
-            System.out.println("프로그램이 종료되었습니다.");
-            return;
-        }
-        System.out.println("첫번째 피연산자를 입력해주세요>> ");
-        p1 = sc.nextInt();
-        System.out.println("두번째 피연산자를 입력해주세요>> ");
-        p2 = sc.nextInt();
+            Set<Integer> set = new HashSet<>();
 
-        int result = 0;
-            switch (operator) {
-                case 1:
-                    result=p1+p2;
-                    System.out.println(p1 + " + " + p2 + " = " + result);
-                    break;
-                case 2:
-                    result=p1-p2;
-                    System.out.println(p1 + " - " + p2 + " = " + result);
-                    break;
-                case 3:
-                    result=p1*p2;
-                    System.out.println(p1 + " * " + p2 + " = " + result);
-                    break;
-                case 4:
-                    if (p2 != 0) {
-                        result = p1 / p2;
-                        System.out.println(p1 + " / " + p2 + " = " + result);
-                    } else {
-                        return;
-                    }
-                    break;
-                case 5:
-                    result=p1%p2;
-                    System.out.println(p1 + " % " + p2 + " = " + result);
-                    break;
-                default:
-                    break;
+            while (set.size() < 3) {
+                int randomNum = (int) (Math.random() * 9) + 1; // 1부터 9 사이의 랜덤 숫자 생성
+                set.add(randomNum);
+            }
+
+            StringBuilder result = new StringBuilder();
+            for (int n : set) {
+                result.append(n);
+            }
+
+            System.out.println(result.toString());
+
+            int Strike = 0, Ball = 0, Out = 0;
+            for (int i = 0; i < 3; i++) {
+                char c = num.charAt(i);
+                if (result.toString().charAt(i) == c) {
+                    Strike++;
+                } else if (result.toString().contains(String.valueOf(c))) {
+                    Ball++;
+                } else {
+                    Out++;
+                }
+            }
+            System.out.println("Strike: " + Strike + "\nBall: " + Ball + "\nOut: " + Out);
+            if (Strike == 3) {
+                System.out.println("정답입니다!");
             }
         }
     }
 }
+
